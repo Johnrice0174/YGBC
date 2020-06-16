abd948c20a14463fda15ca099a5ed6441dd2e1d9
Johnrice0174 <johnrice.rapper@gmail.com>
2020-06-16 18:33:26 +0300

    Updates

---
+++ precache-manifest.07c55941b12c35c8cab59ce9dc2a659d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967d1c6901d082daf28d41b41e7fec74",
+    "url": "/YGBC/index.html"
+  },
+  {
+    "revision": "f59c8b1403bb855b3767",
+    "url": "/YGBC/static/css/2.fd8abb92.chunk.css"
+  },
+  {
+    "revision": "00b0b78b2c3799f5f77d",
+    "url": "/YGBC/static/css/main.eae2218f.chunk.css"
+  },
+  {
+    "revision": "f59c8b1403bb855b3767",
+    "url": "/YGBC/static/js/2.fca37685.chunk.js"
+  },
+  {
+    "revision": "43e7ad3368a4027b5a0b795611ddeb89",
+    "url": "/YGBC/static/js/2.fca37685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b0b78b2c3799f5f77d",
+    "url": "/YGBC/static/js/main.3cea2dc4.chunk.js"
+  },
+  {
+    "revision": "e469beb22d1cec241621",
+    "url": "/YGBC/static/js/runtime-main.b7599997.js"
+  },
+  {
+    "revision": "f05e37d5dde17938e5efbd6ff5876374",
+    "url": "/YGBC/static/media/answer.f05e37d5.png"
+  },
+  {
+    "revision": "8b11aa76736b8af8552c0d89ba091f62",
+    "url": "/YGBC/static/media/background.8b11aa76.jpg"
+  },
+  {
+    "revision": "c9fa9aa78955ed8f5ef7e79ff459b3ec",
+    "url": "/YGBC/static/media/button_sound.c9fa9aa7.mp3"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/YGBC/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "00c33ecf36108d62651ddfd973d98ae6",
+    "url": "/YGBC/static/media/correct_answer.00c33ecf.mp3"
+  },
+  {
+    "revision": "2dcce27160495d68abf4945acd282448",
+    "url": "/YGBC/static/media/materialdesignicons-webfont.2dcce271.woff"
+  },
+  {
+    "revision": "3e2c1c7919fb45d5dee5b0703fe52931",
+    "url": "/YGBC/static/media/materialdesignicons-webfont.3e2c1c79.eot"
+  },
+  {
+    "revision": "a323c28ecd42189e70efca274f5b7089",
+    "url": "/YGBC/static/media/materialdesignicons-webfont.a323c28e.woff2"
+  },
+  {
+    "revision": "e7dec9c5e1bd830c084f2d2fb94fa1e7",
+    "url": "/YGBC/static/media/materialdesignicons-webfont.e7dec9c5.ttf"
+  },
+  {
+    "revision": "0ab14d143f1993fd0a065be0e0aa58a5",
+    "url": "/YGBC/static/media/options.0ab14d14.png"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/YGBC/static/media/warm-bg.635ed87d.jpg"
+  },
+  {
+    "revision": "bc4ec5f11b62240fe1005c33237b85f1",
+    "url": "/YGBC/static/media/wrong_answer.bc4ec5f1.mp3"
+  }
+]);
